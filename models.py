282efb1b52c820a8896e246a44c87044b4f5aa9a
Rusty <rust.roberts@protonmail.com>
2019-06-22 12:05:06 -0500

    removed old models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-# Built-in models for peak fitting from lmfit
-
-from lmfit.models import LorentzianModel, GaussianModel, \
-    PseudoVoigtModel, LinearModel
-
-
-def gaussian(x, amp, center, sigma):
-    return()
