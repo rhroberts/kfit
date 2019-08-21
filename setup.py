420c7bd75c2d54093a5c0245f349e15595d95c46
rhroberts <rust.roberts@protonmail.com>
2019-08-21 09:06:03 -0500

    updated snapcraft.yaml

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
+
 from setuptools import setup, find_packages
 
+DEST = 'share/kfit/'
 
 setup(
         name='kfit',
         version='0.1.0',
         description='Simple, graphical spectral fitting in Python.',
+        author="Rusty Roberts",
+        license="GNU GPL v3",
+        data_files=[(DEST, ['kfit/kfit.py'])],
         packages=find_packages(),
         install_requires=[
             'pandas',
@@ -14,5 +20,5 @@ setup(
             'lmfit',
             'PyQt5',
             'pyperclip'
-    ]
+        ]
 )
