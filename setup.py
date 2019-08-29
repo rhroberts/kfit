ddf15a1ee4c16d45912b71b52eeb1357542d19b7
Rusty Roberts <rust.roberts@protonmail.com>
2019-08-28 21:05:33 -0500

    added kfit.desktop, updated setup.py and snapcraft.yaml

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 from setuptools import setup, find_packages
 
+prime = './'
 app_dir = 'share/kfit/'
 image_dir = 'share/images/'
 
@@ -12,10 +13,18 @@ setup(
         author="Rusty Roberts",
         license="GNU GPL v3",
         data_files=[
+            (prime, ['kfit.desktop']),
             (app_dir, ['kfit/kfit.py', 'kfit/models.py', 'kfit/tools.py',
                        'kfit/kfit.glade', 'kfit/kfit.mplstyle',
                        'kfit/custom_backend_gtk3.py']),
-            (image_dir, ['images/kfit_v2.svg'])
+            (image_dir, ['images/kfit_v2.svg',
+                         'images/document-properties-symbolic.svg',
+                         'images/document-save-symbolic.svg',
+                         'images/document-send-symbolic.svg',
+                         'images/drive-optical-symbolic.svg',
+                         'images/view-refresh-symbolic.svg',
+                         'images/list-add-symbolic.svg',
+                         'images/list-remove-symbolic.svg'])
         ],
         packages=find_packages(),
         install_requires=[
